531eb8f984752e4b166321f494170c46611e9178
Md. Khatami <67198296+khatamirock@users.noreply.github.com>
2023-01-23 10:50:17 +0600

    Update equationgallery.py

--- equationgallery.py
+++ equationgallery.py
@@ -1,35 +1,15 @@
-import sympy as sp
-from sympy import symbols, Eq, solve, cos, pi, tan, sin, atan
-from mpmath import radians, degrees
-from sympy.parsing.sympy_parser import parse_expr
-from equMain import *
-
- 
-
 
 class equclass:
 
-    def __init__(self, leftside, rightside, variableList=[]):
-        self.eq = Eq(leftside, rightside)
-        self.variableList = list(self.eq.free_symbols)
-
-    def eqsolve(self, what):
-        ret = solve(self.eq, what)
-        print(self.eq, what, ret)
-        if len(ret) == 2:
-            ret = ret[1]
-        else:
-            ret = ret[0]
-
-        rets = ret.evalf(3, subs=globalsvar)
-        return rets
-
+    def __init__(self, s1, s2, lister=[]):
+        self.eq = Class(s1, s2)
+        lister = list(self.list)
  
 
-eq = equclass(p**2+q**2+2*p*q*cos(radians(theta)), r**2)
-eq2 = equclass((p*sin(radians(theta))) /
-               (q+p*cos(radians(theta))), tan(radians(alpha)))
-eq3 = equclass(r*cos(radians(theta)), x)
+eq = equclass(p**2+q**2+2*p*q*(theta)), r**2)
+eq2 = equclass((p*(theta)) /
+               (q+p*(theta)), tan(alpha)))
+eq3 = equclass(r*cos(theta), x)
 eq4 = equclass(u*t+0.5*a*t**2, s)
 eq5 = equclass(v**2, u**2+2*a*s)
 eq6 = equclass(a, (v-u)/2)
